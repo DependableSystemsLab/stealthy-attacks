4ea7eab4019fd8690178ccc697e5b54d57a9a233
Pritam Dash <pdash@karthik-pc12>
2019-09-02 14:01:31 -0700

    adding scripts to run attack and non attack versions

---
+++ startSimulatorWithAttack.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+xhost +SI:localuser:root
+
+case $1 in 
+	copter )
+		vehicle="ArduCopter";;
+	rover )
+		vehicle="APMrover2";;
+	* )
+		echo "Please specify vehcile type (copter or rover). Example: ./startSimulator copter --console --map";
+		exit 0;
+esac
+
+docker run --rm -it --name sitl -e XAUTHORITY=/tmp/xauth -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $(pwd)/logs/$1:/stealthy-attacks/Simulator/ardupilot-attack-version/$vehicle/statelogs/logs -v $(pwd)/build_sitl/$1:/stealthy-attacks/Simulator/ardupilot-attack-version/build/sitl -w /stealthy-attacks/Simulator/ardupilot-attack-version/$vehicle stealthy-attacks:latest sim_vehicle.py --aircraft statelogs ${@:2}
+
+# kill $SOCAT_SCID_PID
