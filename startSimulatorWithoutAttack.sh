ba7694592c85821fc0eca412b15531c175b1c39f
Pritam Dash <pdash@karthik-pc12>
2019-09-02 15:14:21 -0700

    updated the scripts

--- startSimulatorWithoutAttack.sh
+++ startSimulatorWithoutAttack.sh
@@ -12,6 +12,6 @@ case $1 in
 		exit 0;
 esac
 
-docker run --rm -it --name sitl -e XAUTHORITY=/tmp/xauth -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $(pwd)/logs/$1:/stealthy-attacks/Simulator/ardupilot-non-attack-version/$vehicle/statelogs/logs -v $(pwd)/build_sitl/$1:/stealthy-attacks/Simulator/ardupilot-non-attack-version/build/sitl -w /stealthy-attacks/Simulator/ardupilot-non-attack-version/$vehicle stealthy-attacks:latest sim_vehicle.py --aircraft statelogs ${@:2}
+docker run --rm -it --name sitl -e XAUTHORITY=/tmp/xauth -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY -v $(pwd)/logs/$1:/stealthy-attacks/Simulator/ardupilot-non-attack-version/$vehicle/statelogs/logs -v $(pwd)/build_sitl/$1:/stealthy-attacks/Simulator/ardupilot-non-attack-version/build/sitl -w /stealthy-attacks/Simulator/ardupilot-non-attack-version/$vehicle stealthy-attacks:latest ../Tools/autotest/sim_vehicle.py --aircraft statelogs ${@:2}
 
 # kill $SOCAT_SCID_PID
